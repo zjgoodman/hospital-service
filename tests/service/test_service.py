ea761658060c39c5d97e5d08cce55fb432c26602
Zach Goodman <zjgoodman95@gmail.com>
2021-12-05 09:02:27 -0600

    more tests

--- tests/service/test_service.py
+++ tests/service/test_service.py
@@ -41,7 +41,16 @@ def test_get_hospitals_without_filters(mocker: MockerFixture):
     assert len(actual_hospitals) == 276
 
 
-def test_get_hospitals_by_measure_op_31(mocker: MockerFixture):
+# to verify the count, grep the file
+# grep OP_31 tests/service/test-measures-filters.csv | wc -l
+measure_expectations = [
+    ("OP_31", 176),
+    ("OP_22", 177),
+]
+
+
+@pytest.mark.parametrize("measureId,expected_count", measure_expectations)
+def test_get_hospitals_by_measure(mocker: MockerFixture, measureId, expected_count):
     mock_settings: Settings = Settings(
         hospital_info_csv_file_name="tests/service/test-hospital-info-filters.csv",
         hospital_treatment_csv_file_name="tests/service/test-measures-filters.csv",
@@ -49,30 +58,9 @@ def test_get_hospitals_by_measure_op_31(mocker: MockerFixture):
 
     mocker.patch("hospital_service.config.get_settings", return_value=mock_settings)
 
-    measureId: str = "OP_31"
     actual_hospitals: List[Hospital] = get_hospitals_by_criteria(measureId)
 
-    assert len(actual_hospitals) == 176
-    # to verify the count, grep the file
-    # grep OP_31 tests/service/test-measures-filters.csv | wc -l
-    # 176
-
-
-def test_get_hospitals_by_measure_op_22(mocker: MockerFixture):
-    mock_settings: Settings = Settings(
-        hospital_info_csv_file_name="tests/service/test-hospital-info-filters.csv",
-        hospital_treatment_csv_file_name="tests/service/test-measures-filters.csv",
-    )
-
-    mocker.patch("hospital_service.config.get_settings", return_value=mock_settings)
-
-    measureId: str = "OP_22"
-    actual_hospitals: List[Hospital] = get_hospitals_by_criteria(measureId)
-
-    assert len(actual_hospitals) == 177
-    # to verify the count, grep the file
-    # grep OP_22 tests/service/test-measures-filters.csv | wc -l
-    # 177
+    assert len(actual_hospitals) == expected_count
 
 
 score_operators_expectations = [
@@ -101,6 +89,46 @@ def test_get_hospitals_by_score_operator(
     score = 2
     actual_hospitals: List[Hospital] = get_hospitals_by_criteria(
         score=score, score_compare_operator=score_compare_operator
+    )
+
+    assert len(actual_hospitals) == expected_count
+
+
+measure_and_score_expectations = [
+    ("OP_22", "ge", 1, 92),
+    ("OP_22", "ge", 2, 56),
+    ("OP_22", "ge", 3, 22),
+    ("OP_22", "ge", 3, 22),
+    ("OP_22", "ge", 4, 10),
+    ("OP_22", "ge", 5, 5),
+    ("OP_22", "ge", 6, 2),
+    ("OP_22", "ge", 7, 0),
+    ("OP_18b", "ge", None, 177),
+    ("OP_18b", "ge", 50, 150),
+    ("OP_18b", "ge", 100, 140),
+    ("OP_18b", "ge", 150, 75),
+    ("OP_18b", "ge", 200, 32),
+    ("OP_18b", "ge", 250, 4),
+    ("OP_18b", "ge", 300, 0),
+]
+
+
+@pytest.mark.parametrize(
+    "measureId,score_compare_operator,score,expected_count",
+    measure_and_score_expectations,
+)
+def test_get_hospitals_by_measure_and_score(
+    mocker: MockerFixture, measureId, score_compare_operator, score, expected_count
+):
+    mock_settings: Settings = Settings(
+        hospital_info_csv_file_name="tests/service/test-hospital-info-filters.csv",
+        hospital_treatment_csv_file_name="tests/service/test-measures-filters.csv",
+    )
+
+    mocker.patch("hospital_service.config.get_settings", return_value=mock_settings)
+
+    actual_hospitals: List[Hospital] = get_hospitals_by_criteria(
+        score=score, measureId=measureId, score_compare_operator=score_compare_operator
     )
 
     assert len(actual_hospitals) == expected_count
